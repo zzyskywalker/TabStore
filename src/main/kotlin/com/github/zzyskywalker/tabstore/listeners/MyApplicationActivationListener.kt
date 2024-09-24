3edb09114d256db4cd76f8d976f60b2ecdc569cb
GitHub Action <action@github.com>
2024-09-24 12:36:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/zzyskywalker/tabstore/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/zzyskywalker/tabstore/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zzyskywalker.tabstore.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
